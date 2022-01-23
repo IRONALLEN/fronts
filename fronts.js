8dc4081b5a100b26d3d185034fd8758f8dac815f
Jean Gonzalez <87197368+IRONALLEN@users.noreply.github.com>
2022-01-23 10:05:15 -0500

    Add files via upload

---
+++ fronts.js
@@ -0,0 +1,49 @@
+class SLLNode {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+class SLL {
+  constructor() {
+    this.head = null;
+  }
+
+  addFront(value) {
+    var newNode = new SLLNode(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    return this.head;
+  }
+  removeFront() {
+    if (this.head == null) {
+      return this.head;
+    }
+    var removedNode = this.head;
+    this.head = removedNode.next;
+    removedNode.next = null;
+    return this.head;
+  }
+  front() {
+    if (this.head == null) {
+      return null;
+    } else {
+      return this.head.value;
+    }
+    return this.head ? this.head.value : null;
+  }
+}
+
+var mySLL = new SLL();
+console.log(mySLL.front());
+mySLL.addFront(10);
+console.log(mySLL.front());
+mySLL.addFront(5);
+mySLL.addFront(3);
+
+// console.log(mySLL);
+
+// mySLL.removeFront();
+
+// console.log(mySLL);
